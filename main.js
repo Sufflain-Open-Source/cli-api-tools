116e10ae9bca5af2ddb07b6dde27a965ae91a984
Timofey Chuchkanov <crt0r.9@yahoo.com>
2022-04-15 19:54:50 +0300

    feat(main.js): implement data encryption and decryption

--- main.js
+++ main.js
@@ -4,4 +4,41 @@
     Licensed under the Sufflain Private Module License.
  */
 
+import config from '../../cli-config.js';
+import * as pgp from 'openpgp';
 
+const pass = 'c6ljzyboxj8Y2cdiPs2Rp9mxkDTQrtwa';
+
+// decryptServerResponse :: String -> String
+export async function decryptServerResponse(response, privateKey) {
+    const payload = response.payload;
+    const { data: decrypted } = await pgp.decrypt({
+        message: await pgp.readMessage({ armoredMessage: payload }),
+        decryptionKeys: await pgp.decryptKey({ privateKey: await pgp.readPrivateKey({ armoredKey: privateKey }), passphrase: pass})
+    });
+
+    return decrypted;
+}
+
+// encryptOwnPubKey :: String -> String
+export async function encryptOwnPubKey(serverPub, ownPub) {
+    const dataToShare = `${ config.shared }@${ ownPub }`;
+    const encryptedPayload = await pgp.encrypt({
+        message: await pgp.createMessage({ text: dataToShare }),
+        encryptionKeys: await pgp.readKey({ armoredKey: serverPub })
+    });
+
+    return encryptedPayload;
+}
+
+// generateKeyPair :: -> undefined
+export async function generateKeyPair() {
+    const { privateKey, publicKey } = await pgp.generateKey({
+        type: 'rsa',
+        rsaBits: 2048,
+        userIDs: [{ name: 'Timofey Chuchkanov', email: 'crt0r.9@yahoo.com' }],
+        passphrase: pass
+    });
+    
+    return { pri: privateKey, pub: publicKey };
+}
