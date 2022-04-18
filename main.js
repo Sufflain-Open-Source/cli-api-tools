495c9b5ebf7a1f09ff60b47b8fa2888aacd00589
Timofey Chuchkanov <crt0r.9@yahoo.com>
2022-04-18 13:48:34 +0300

    fix(main.js): improve pages loading speed by removing response encryption

--- main.js
+++ main.js
@@ -4,50 +4,23 @@
     Licensed under the Sufflain Private Module License.
  */
 
-import config from '../../cli-config.js';
 import { fetchServerPubKey } from '../data/remote.js';
-import { setServerPubKey, setKeyPair } from '../data/session.js';
+import { setServerPubKey } from '../data/session.js';
 import * as pgp from 'openpgp';
-
-const pass = 'c6ljzyboxj8Y2cdiPs2Rp9mxkDTQrtwa';
 
 // setupEncryption :: -> Undefined
 export async function setupEncryption() {
     const serverPubKey = await fetchServerPubKey();
-    const keyPair = await generateKeyPair(); 
 
     setServerPubKey(serverPubKey);
-    setKeyPair(keyPair);
 }
 
-// decryptServerResponse :: String -> String
-export async function decryptServerResponse(response, privateKey) {
-    const payload = response.payload;
-    const { data: decrypted } = await pgp.decrypt({
-        message: await pgp.readMessage({ armoredMessage: payload }),
-        decryptionKeys: await pgp.decryptKey({ privateKey: await pgp.readPrivateKey({ armoredKey: privateKey }), passphrase: pass})
-    });
-
-    return decrypted;
-}
-
-// encryptOwnPubKey :: String -> String
-export async function encryptOwnPubKey(serverPub, ownPub) {
-    const dataToShare = `${ config.shared }@${ ownPub }`;
+// encryptApiKey :: String -> String
+export async function encryptApiKey(serverPub, apiKey) {;
     const encryptedPayload = await pgp.encrypt({
-        message: await pgp.createMessage({ text: dataToShare }),
+        message: await pgp.createMessage({ text: apiKey }),
         encryptionKeys: await pgp.readKey({ armoredKey: serverPub })
     });
 
     return encryptedPayload;
-}
-
-// generateKeyPair :: -> undefined
-export async function generateKeyPair() {
-    const { privateKey, publicKey } = await pgp.generateKey({
-        userIDs: [{ name: 'Timofey Chuchkanov', email: 'crt0r.9@yahoo.com' }],
-        passphrase: pass
-    });
-    
-    return { pri: privateKey, pub: publicKey };
 }
